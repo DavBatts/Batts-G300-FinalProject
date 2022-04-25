fc50a82594e972509a5ec9f7373cc8fcb11f4234
DavBatts <dsbatts13@gmail.com>
2022-04-24 22:12:19 -0400

    Adds Player movement

---
+++ Final Project/Assets/Scripts/PlayerMovement.cs
@@ -0,0 +1,38 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[RequireComponent(typeof(Animator))]
+[RequireComponent(typeof(Rigidbody2D))]
+public class PlayerMovement : MonoBehaviour
+{
+    //necessary for animations and physics
+    private Rigidbody2D rb2D;
+    private Animator myAnimator;
+
+    //Variables to play with
+    public float speed = 2.0f;
+    public float horizMovement;//= 1[OR] -1[OR]0
+
+
+   private void Start()
+    {
+        //define the game objects found on the player
+        rb2D = GetComponent<Rigidbody2D>();
+        myAnimator = GetComponent<Animator>();
+    }
+
+    //handles the input for physics
+   private void Update()
+    {
+        //check direction given to player
+        horizMovement = Input.GetAxisRaw("Horizontal");
+    }
+
+    //handles running the physics
+    private void FixedUpdate()
+    {
+        //move the character
+        rb2D.velocity = new Vector2(horizMovement*speed,rb2D.velocity.y);
+    }
+}
